e0891f67f1b5c6f2aacccc267e2f8f4db73768cd
Martin Westergaard Lassen <martin@mwl.dk>
2013-02-11 11:24:16 +0100

    Added ability to inspect buckets

--- Makefile
+++ Makefile
@@ -2,4 +2,6 @@
 install:
 	@curl -XPUT -H "Content-Type: text/html" --data @src/index.html http://localhost:8098/riak/ngriakadmin/index.html
 	@curl -XPUT -H "Content-Type: text/html" --data @src/riak.html http://localhost:8098/riak/ngriakadmin/riak.html
+	@curl -XPUT -H "Content-Type: text/html" --data @src/buckets.html http://localhost:8098/riak/ngriakadmin/buckets.html
+	@curl -XPUT -H "Content-Type: text/html" --data @src/bucket.html http://localhost:8098/riak/ngriakadmin/bucket.html
 	@curl -XPUT -H "Content-Type: text/javascript" --data @src/ngriakadmin.js http://localhost:8098/riak/ngriakadmin/ngriakadmin.js
--- src/ngriakadmin.js
+++ src/ngriakadmin.js
@@ -2,12 +2,25 @@ angular.module('riakadmin', []).
     config(function($routeProvider) {
         $routeProvider.
             when('/riak', {controller:RiakCtrl, templateUrl:'riak.html'}).
+            when('/buckets', {controller:BucketsCtrl, templateUrl:'buckets.html'}).
+            when('/buckets/:bucket', {controller:BucketCtrl, templateUrl:'bucket.html'}).
             otherwise({redirectTo:'/riak'});
     });
 
-function RiakCtrl($scope, $http) {
+function RiakCtrl($scope) {
+}
+
+function BucketsCtrl($scope, $http) {
     $http({method: 'GET', url:'/riak?buckets=true'}).
         success(function(data, status, headers, config) {
             $scope.buckets = data.buckets;
         });
+}
+
+function BucketCtrl($scope, $routeParams, $http) {
+    $http({method: 'GET', url:'/riak/' + $routeParams.bucket + '?keys=true'}).
+        success(function(data, status, headers, config) {
+            $scope.props = data.props;
+            $scope.keys = data.keys;
+        })
 }
